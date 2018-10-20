af78d16bc89dafea5f0132f5eb0be13e9afbb0dd
Jakub Jalowiec <jjalowie@mion.elka.pw.edu.pl>
2018-10-20 13:15:36 +0200

    fixed actions

--- parser.sh
+++ parser.sh
@@ -1,29 +1,36 @@
+declare -A patterns
+declare -A actions
 
 create()
 {
     echo "create $1 <- from function"
 }
 
+listall()
+{
+    echo "->>>>listall"
+}
+
 match(){
     echo "/$1/ {print $2}" > awk.txt
     cmd=$(awk -f awk.txt input.txt)
-    echo "cmd: >$cmd<"
+#    echo "cmd: >$cmd<"
     eval $cmd
 }
 
-declare -A patterns
-declare -A actions
 i=1
-
 while read pattern;
 do
     read action
-    action="\"$action\""
-    a1=`echo $action | sed 's/\$[[:digit:]]/\\"&\\"/g'`
-    echo "a1 $a1"
-    echo "$pattern -> $action"
-    patterns[$i]=$pattern
-    actions[$i]=$a1
+    # add quotes to literals to print in awk
+    # & is the matched string
+    {
+
+        action="\"$action\""
+        action=`echo $action | sed 's/\$[[:digit:]]/\\"&\\"/g'`
+        patterns[$i]=$pattern
+        actions[$i]=$action
+    }
     i=$((i+1))
 done < cmd.txt
 
@@ -32,9 +39,9 @@ pattern_matching()
     k=1
     for i in "${!patterns[@]}"
     do
-        echo "i: $i match ${patterns[$i]}: ${actions[$i]}"
+#        echo "i: $i match ${patterns[$i]}: ${actions[$i]}"
         match "${patterns[$i]}" "${actions[$i]}"
-        echo "^^"
+#        echo "^^"
     done
 }
 
